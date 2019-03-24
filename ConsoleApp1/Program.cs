d5f5bdb04f1e61fbcdb1e8c5846c47ddaf6edca7
Will <doggy.huang@gmail.com>
2019-03-24 12:03:39 +0800

    06 練習透過 Entity Framework 對資料進行完整的 CRUD 操作

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,7 +13,12 @@ namespace ConsoleApp1
         {
             var db = new ContosoUniversityEntities();
 
-            //db.Database.Log = (msg) => Console.WriteLine(msg);
+            db.Database.Log = (msg) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(msg);
+                Console.ResetColor();
+            };
 
             //SelectCourseByGitOrderByCredits(db);
 
@@ -21,7 +26,66 @@ namespace ConsoleApp1
             //Console.WriteLine("--");
             //SelectDepartmentWithRelation(db);
 
+            PracticeCRUD(db);
 
+
+        }
+
+        private static void PracticeCRUD(ContosoUniversityEntities db)
+        {
+            var dept = new Department()
+            {
+                Name = "酷奇資訊",
+                Budget = 18000,
+                StartDate = new DateTime(2019, 6, 1, 0, 0, 0)
+            };
+
+            dept.Course.Add(new Course()
+            {
+                Title = "ASP.NET MVC 5 開發實戰",
+                Credits = 1,
+                Department = dept
+            });
+
+            dept.Course.Add(new Course()
+            {
+                Title = "Angular 7 開發實戰",
+                Credits = 1,
+                Department = dept
+            });
+
+            Console.WriteLine("------ 新增部門與課程資料 ------");
+            db.Department.Add(dept);
+            db.SaveChanges();
+
+            Console.WriteLine("------ 取得新增後的 DepartmentID ------");
+
+            Console.WriteLine("新部門 ID = " + dept.DepartmentID);
+
+            Console.WriteLine("------ 重新查詢之前新增的部門資料 ------");
+
+            var new_dept = db.Department.Find(dept.DepartmentID);
+
+            Console.WriteLine("部門名稱: " + new_dept.Name);
+
+            Console.WriteLine("------ 查詢該部門所有課程資料 ------");
+
+            foreach (var course in new_dept.Course)
+            {
+                course.Credits = 5;
+                Console.WriteLine("\t" + course.Title + "\t" + course.Credits);
+            }
+
+            Console.WriteLine("------ 更新該部門所有課程的 Credits 分數 ------");
+
+            db.SaveChanges();
+
+            db.Course.RemoveRange(new_dept.Course);
+            db.Department.Remove(new_dept);
+
+            Console.WriteLine("------ 移除課程資料與部門資料 ------");
+
+            db.SaveChanges();
         }
 
         private static void SelectCourseByGitOrderByCredits(ContosoUniversityEntities db)
